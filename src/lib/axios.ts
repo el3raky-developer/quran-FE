760b0a5b0f5c5592750ab02c6a127d68bf81b4eb
el3raky <kameladel339@gmail.com>
2026-03-03 17:43:20 +0200

    Switch API base URL in axios configuration to use the onrender.com endpoint for production, while commenting out the previous production URL.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://quran-be-production.up.railway.app";
-// const API_BASE_URL = "https://quran-be-6rgt.onrender.com";
+// const API_BASE_URL = "https://quran-be-production.up.railway.app";
+const API_BASE_URL = "https://quran-be-6rgt.onrender.com";
 // const API_BASE_URL = "http://localhost:5000";
 
 const api = axios.create({
